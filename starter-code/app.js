a1717aa9fda0e38e94395a015f5ee3f8a4dd6f47
Francisco Horak <horakfrancisco@gmail.com>
2020-04-12 20:12:07 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
-const path = require('path');
+var path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,8 +13,38 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render("index"));
+
+app.get('/beers', (req, res) => {
+    const allBeers = punkAPI.getBeers();
+    allBeers.then(allBeers => {
+        res.render("beers", {
+            allBeers
+        });
+    })
+})
+
+app.get('/random-beers', (req, res) => {
+    const randomBeer = punkAPI.getRandom()
+    randomBeer.then(randomBeer => {
+        res.render("random-beers", {
+            randomBeer
+        })
+    })
+});
+
+
+
+app.get('/beers/:id', (req, res) => {
+    const beerID = req.params.id;
+    const beerInfo = punkAPI.getBeer(beerID)
+    beerInfo.then(beerInfo => {
+        res.render('beer-Info', {
+            beerInfo
+        })
+    })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
